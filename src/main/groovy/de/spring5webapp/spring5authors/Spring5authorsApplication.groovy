cdfa0eef6ce98b75528c458dd1072f26898298b4
VSV <viktor.stoitschev@westernacher.com>
2018-07-31 23:19:32 +0200

    First commit

---
+++ src/main/groovy/de/spring5webapp/spring5authors/Spring5authorsApplication.groovy
@@ -0,0 +1,12 @@
+package de.spring5webapp.spring5authors
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Spring5authorsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run Spring5authorsApplication, args
+	}
+}
---
+++ src/test/groovy/de/spring5webapp/spring5authors/Spring5authorsApplicationTests.groovy
@@ -0,0 +1,16 @@
+package de.spring5webapp.spring5authors
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class Spring5authorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
