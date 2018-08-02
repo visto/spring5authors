d56e46f4f72686e4b10535e87ed5b20135918da6
VSV <viktor.stoitschev@westernacher.com>
2018-08-02 14:49:17 +0200

    Add the books thymeleaf template

--- src/main/groovy/de/spring5webapp/spring5authors/model/Book.groovy
+++ src/main/groovy/de/spring5webapp/spring5authors/model/Book.groovy
@@ -12,12 +12,12 @@ class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id
-    private String title
-    private String isbn
+    Long id
+    String title
+    String isbn
 
     @OneToOne
-    private Publisher publisher
+    Publisher publisher
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
